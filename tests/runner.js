6395fe7a26fe3315a06c9dbd7cd5311c7d778254
Iwan <valcke.maxim@gmail.com>
2019-04-01 20:15:51 +0200

    Make filename in test output reproducible between different machine in tests

--- tests/runner.js
+++ tests/runner.js
@@ -10,6 +10,24 @@ function parseFile(filename, recover) {
   args.push(filename);
   return cp.spawnSync(parser, args);
 }
+// Parse error: /home/travis/build/IwanKaramazow/napkinscript/tests/parsing/errors/scanner/oldDerefOp.js, line: 1, col: 50
+// test output contains the full path of the file
+// this differs between multiple machines
+// Just drop "/home/travis/build/IwanKaramazow" to make the file path machine independent
+let makeReproducibleFilename = txt => {
+  // "Parse error: "
+  let lines = txt.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let txt = lines[i];
+    if (txt.indexOf("Parse error") === -1) {
+      continue;
+    }
+    let prefix = txt.substring(0, 13);
+    let suffix = txt.substring(txt.indexOf("/napkinscript"), txt.length);
+    lines[i] = prefix + suffix;
+  }
+  return lines.join("\n");
+};
 
 global.runParser = (dirname, recover = false, showError = false) => {
   fs.readdirSync(dirname).forEach(base => {
@@ -26,7 +44,7 @@ global.runParser = (dirname, recover = false, showError = false) => {
         output += `=====Parsetree==========================================\n`;
         output += `${parsetree}\n`;
         output += `=====Errors=============================================\n`;
-        output += `${res.stderr.toString()}\n`;
+        output += `${makeReproducibleFilename(res.stderr.toString())}\n`;
         output += `========================================================`;
       } else {
         output = parsetree;
